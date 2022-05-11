dcdb3f6252efbaccbf3480e8a21102241633b1b5
Narendra Samtani <alenway97@gmail.com>
2022-05-11 08:01:53 +0530

    monday

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import Expenses from "./components/Expenses";
+import Expenses from "./components/Expenses/Expenses";
 import "./App.css";
 
-function App() {
+const App = () => {
   const expenses = [
     {
       id: "e1",
@@ -34,6 +34,6 @@ function App() {
       <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
